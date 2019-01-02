3ecfb0ce5fc79f3328002b5a9248aed6e8d81ba4
Артур Соловьев < >
2019-01-02 21:31:15 +0300

    Added StringTools class and completed Split method

---
+++ include/StringTools.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Octavia on 02.01.2019.
+//
+
+#ifndef SHUNTING_YARD_STRINGTOOLS_H
+#define SHUNTING_YARD_STRINGTOOLS_H
+
+#include <vector>
+#include <string>
+
+namespace cs
+{
+	namespace StringTools
+	{
+		std::vector<std::string> Split(std::string _source, std::string _delims = " ");
+	};
+}
+
+#endif //SHUNTING_YARD_STRINGTOOLS_H
---
+++ src/StringTools.cpp
@@ -0,0 +1,56 @@
+//
+// Created by Octavia on 02.01.2019.
+//
+
+#include <StringTools.hpp>
+
+std::vector<std::string> cs::StringTools::Split(std::string _source, std::string _delims)
+{
+	enum State
+	{
+		OutsideToken,
+		InsideToken
+	};
+
+	State state = OutsideToken;
+	std::size_t currentTokenStartIndex;
+
+	std::vector<std::string> parsedTokens;
+
+	for(std::size_t i = 0; i < _source.size(); i++)
+	{
+		bool isDelimeter = false;
+
+		if(_delims.find(_source[i]) != std::string::npos)
+		{
+			isDelimeter = true;
+		}
+
+		switch(state)
+		{
+			case OutsideToken:
+			{
+				if(isDelimeter) // We are outside token and next symbol is not a token part, skip it
+				{
+					break;
+				}
+				currentTokenStartIndex = i;
+				state = InsideToken;
+				break;
+			}
+			case InsideToken:
+			{
+				if(!isDelimeter) // We are inside token and next symbol is not a token end, skip
+				{
+					break;
+				}
+				// Take a substring from the current token beginning to the current position (i). Token parsed.
+				parsedTokens.emplace_back(_source.substr(currentTokenStartIndex, i - currentTokenStartIndex));
+				state = OutsideToken;
+				break;
+			}
+		}
+	}
+
+	return parsedTokens;
+}
