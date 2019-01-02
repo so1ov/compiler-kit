9d62aa6a7025959e09d60bd3e73fa4ae8af77a25
Артур Соловьев < >
2019-01-02 23:09:34 +0300

    Main test: improved output

--- test.cpp
+++ test.cpp
@@ -5,18 +5,18 @@
 
 int main()
 {
-	std::string infix = "3 + 4 + 5 - 3";
+	std::string infix = "3 + 4.1 * 5 - 3 / 1";
+	std::cout << "Infix string: " << infix << std::endl;
 
 	cs::ExpressionParser parser;
 
 	auto postfixTokens = parser.FromInfixToPostfix(infix);
 
+	std::cout << "Postfix string: ";
 	for(auto& it : postfixTokens)
 	{
 		std::cout << it.Body << ' ';
 	}
 	std::cout << std::endl;
-
-	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
